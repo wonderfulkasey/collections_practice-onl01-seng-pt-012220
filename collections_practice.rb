472f7c927378c85e2d9ec2699edfd96cb0b83854
Kasey Arnold <kaseyarnold15@gmail.com>
2020-01-30 22:40:34 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -3,34 +3,34 @@ def sort_array_asc(array)
   array.sort
 end
 
-def sort_array_desc
+def sort_array_desc(array)
 #.sort
 end
 
-def sort_array_char_count
+def sort_array_char_count(array)
 #.sort
 end
 
-def swap_elements
+def swap_elements(array)
 
 end
 
-def reverse_array
+def reverse_array(array)
   
 end
 
-def kesha_maker
+def kesha_maker(array)
 #.each
 end
 
-def find_a
+def find_a(array)
 #start_with? "a"
 end
 
-def sum_array
+def sum_array(array)
 #.inject
 end
 
-def add_s
+def add_s(array)
 #.each_with_index .collect
 end 
